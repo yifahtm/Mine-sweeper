d91a84901931415e7daed14b0ac7b97f30b346bd
yifahtm <159137217+yifahtm@users.noreply.github.com>
2024-02-08 21:06:33 +0200

    Final upload
    The Randomality worked after the play again is clicked. but then i did some changes and went back and it didnt work anymore. Then i only had 4-10 minutes left to finish. I'm hoping an earlier version of my work is saved on github. i'm hoping it will make a difference.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 'use strict'
+// The Randomality worked after the play again is clicked. but then i did some changes and went back and it didnt work anymore. Then i only had 4-10 minutes left to finish. I'm hoping an earlier version of my work is saved on github. i'm hoping it will make a difference.
+
 const MINE = '💣'
 const FLAG = '🚩'
 const EMPTY = ''
@@ -12,7 +14,7 @@ var gLevels = [
 ]
 
 var gGame = {
-    isOn: true,
+    isOn: false,
     shownCount: 0,
     markedDown: 0,
     secsPassed: 0,
@@ -23,6 +25,10 @@ function onInit() {
     gBoard = buildBoard()
     renderBoard(gBoard)
     gGame.isOn = false
+    gGame.shownCount = 0
+    gGame.markedDown = 0
+    gGame.secsPassed = 0
+    gGame.isVictory = false
     var elMinesInSpan = document.querySelector('.mines-container .mines-in');
     elMinesInSpan.innerText = gLevels[0].mines
 }
@@ -31,8 +37,6 @@ function onSetLevel(size, mines) {
     console.log(size, mines)
     gLevels[0].size = size
     gLevels[0].mines = mines
-    console.log(gLevels[0].size, gLevels[0].mines)
-
     onInit()
 }
 
@@ -44,10 +48,9 @@ function buildBoard() {
             board[i][j] = createCell(i, j)
         }
     }
-    puttingMines(board)
-    //RandomPuttingMines(board)
+    //puttingMines(board)
+    RandomPuttingMines(board)
     setMinesNegsCount(board)
-
     return board
 }
 
@@ -75,15 +78,10 @@ function renderBoard(board) {
             } else {
                 cellDisplay = cell.minesAroundCount
             }
-
-
             strHTML += `<td data-i=${i} data-j=${j} onclick="onCellClicked(this, ${i}, ${j})" oncontextmenu="onCellMarked(this, ${i}, ${j})" class="${className} "><span class="hidden">${cellDisplay}</span></td>`
-
         }
         strHTML += '</tr>'
-
     }
-
     const elBoard = document.querySelector('tbody.board')
     elBoard.innerHTML = strHTML
 }
@@ -92,13 +90,10 @@ function renderBoard(board) {
 function puttingMines(board) {
     board[0][2].isMine = true
     board[2][3].isMine = true
-
 }
 
 //Randomly set mines
 function RandomPuttingMines(board) {
-    console.log('grapejuice soda')
-
     while (gLevels[0].mines > 0) {
         const randRowIdx = getRandomInt(0, gLevels[0].size)
         const randColIdx = getRandomInt(0, gLevels[0].size)
@@ -106,16 +101,12 @@ function RandomPuttingMines(board) {
             var cell = board[randRowIdx][randColIdx]
             cell.isMine = true
             gLevels[0].mines--
-            console.log(`setMinesOnBoard: Mine placed at [${randRowIdx}][${randColIdx}]`)
         }
     }
-    console.log(board)
-    console.log(gLevels[0].mines)
 }
 
 function getMinesNegsCount(cellI, cellJ, board) {
     var mineCount = 0
-
     for (var i = cellI - 1; i <= cellI + 1; i++) {
         if (i < 0 || i >= board.length) continue
         for (var j = cellJ - 1; j <= cellJ + 1; j++) {
@@ -124,10 +115,8 @@ function getMinesNegsCount(cellI, cellJ, board) {
             if (board[i][j].isMine) mineCount++
         }
     }
-
     return mineCount
 }
-
 
 function setMinesNegsCount(board) {
     for (var i = 0; i < board.length; i++) {
@@ -139,7 +128,6 @@ function setMinesNegsCount(board) {
             } else {
                 cell.minesAroundCount = EMPTY
             }
-            // console.log (`setMinesNegsCount: cell [${i}][${j}] minesAroundCount: ${cellMinesNegsCount}`)
         }
     }
 }
@@ -151,20 +139,19 @@ function onCellClicked(elCell, cellI, cellJ) {
     if (elCell.classList.contains('marked')) return
     var elCellSpan = elCell.querySelector('span')
     var cell = gBoard[cellI][cellJ]
-
+    var expectedCount = (gLevels[0].size ** 2) - gLevels[0].mines
     elCell.classList.add('clicked')
     elCellSpan.classList.remove('hidden')
     cell.isShown = true
     gGame.shownCount++
     if (gGame.shownCount === 1) startTimer()
-    if (gGame.shownCount === 14) {
+    //Works, only if i do not put the FLAGS on the first and the last moves. If i do, i have to remove the second condition
+    if (gGame.shownCount === expectedCount && gGame.markedDown === gLevels[0].mines) {
         // && gGame.markedDown === gLevels[0].mines
         clearInterval(gTimerInterval)
     }
     expandShown(gBoard, elCell, cellI, cellJ)
-    checkGameOver(elCell, cellI, cellJ)
-    console.log(cell.isShown)
-    console.log(gGame.shownCount)
+    checkGameOver(cellI, cellJ)
 }
 
 function startTimer() {
@@ -181,7 +168,6 @@ function startTimer() {
         document.querySelector('span.minutes').innerText = minutes
         document.querySelector('span.seconds').innerText = seconds
         document.querySelector('span.milli-seconds').innerText = milliSeconds
-
     }, 10)
 }
 
@@ -201,7 +187,6 @@ function getFormatMilliSeconds(timeDiff) {
 }
 
 function onCellMarked(elCell, cellI, cellJ) {
-    console.log(elCell)
     var elMinesMarkedSpan = document.querySelector('.mines-container .mines-marked')
     // var elCell = document.querySelector(`.cell-${cellI}-${cellJ}`)
     var cell = gBoard[cellI][cellJ]
@@ -211,15 +196,11 @@ function onCellMarked(elCell, cellI, cellJ) {
         cell.isMarked = true
         gGame.markedDown++
         elMinesMarkedSpan.innerText = gGame.markedDown
-        console.log(cell.isMarked)
-        console.log(gGame.markedDown)
     } else {
         elCell.classList.remove('marked')
         cell.isMarked = false
         gGame.markedDown--
         elMinesMarkedSpan.innerText = gGame.markedDown
-        console.log(cell.isMarked)
-        console.log(gGame.markedDown)
     }
     //Marking and Unmarking works, but erases all content that was there before and FLAG remains
 
@@ -231,22 +212,22 @@ function onCellMarked(elCell, cellI, cellJ) {
     // }
 }
 
-function checkGameOver(elCell, cellI, cellJ) {
+function checkGameOver(cellI, cellJ) {
     var cell = gBoard[cellI][cellJ]
     var expectedCount = (gLevels[0].size ** 2) - gLevels[0].mines
-    console.log(expectedCount)
     if (cell.isMine) {
         gameOver()
     } else {
-        if (gGame.shownCount === expectedCount) {
+        //Works, only if i do not put the FLAGS on the first and the last moves. If i do, i have to remove the second condition
+        if (gGame.shownCount === expectedCount && gGame.markedDown === gLevels[0].mines) {
             //&& gGame.markedDown === gLevels[0].mines
             victory()
         }
-        console.log(gGame.markedDown)
     }
 }
 
 function gameOver() {
+    clearInterval(gTimerInterval)
     gGame.isOn = false
     gGame.isVictory = false
     gGame.shownCount = 0
@@ -256,6 +237,7 @@ function gameOver() {
 }
 
 function victory() {
+    clearInterval(gTimerInterval)
     gGame.isOn = false
     gGame.isVictory = true
     gGame.shownCount = 0
@@ -271,9 +253,11 @@ function onPlayAgain() {
     gGame.shownCount = 0
     gGame.markedDown = 0
     gGame.secsPassed = 0
+    gBoard = buildBoard()
+    RandomPuttingMines(gBoard)
+    setMinesNegsCount(gBoard)
     renderBoard(gBoard)
-    onInit()
-    hideModal()
+    gGame.isOn = true
 }
 
 function onRestart() {
@@ -288,47 +272,41 @@ function onRestart() {
 }
 
 function showModal() {
-
     const elModal = document.querySelector('.modal')
-    const elModalMsg = elModal.querySelector('.modal .res-message')
+    const elModalMsg = elModal.querySelector('.res-message')
     elModal.classList.remove('hide')
-
     if (gTimerInterval) clearInterval(gTimerInterval)
-
     if (gGame.isVictory) elModalMsg.innerText = 'Victorious!'
-
     if (!gGame.isVictory) elModalMsg.innerText = 'Game Over!'
 }
 
 function hideModal() {
     const elModal = document.querySelector('.modal')
-    elModal.classList.add('hidden')
+    elModal.classList.add('hide')
 }
 
-//Not working. Related to onCellMarked?
-function expandShown(board, elcell, cellI, cellJ) {
-    console.log('grapejuice stuff 4va')
-    if (board[cellI][cellJ].minesAroundCount === 0) {
-
-        for (var i = cellI - 1; i <= cellI + 1; i++) {
-            if (i < 0 || i >= board.length) continue
-            for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-                if (j < 0 || j >= board[i].length) continue
-                if (i === cellI && j === cellJ) continue
-                var elNegCells = document.querySelectorAll(`.hidden`)
-                console.log('Selected Element:', elNegCells)
-                for (var k = 0; k < elNegCells.length; k++) {
-                    elNegCells[k].classList.remove('hidden')
-                    console.log('Removed hidden class')
-                }
+//Not working. I tried yesterday, i tried today, went through past classes where we highlight stuff. I checked, its not selecting the cell properly. Tried recursion. Maybe also related to onCellMarked?
+function expandShown(board, elCell, cellI, cellJ) {
+    //Without Recursion
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (i === cellI && j === cellJ) continue
+            if (j < 0 || j >= board[0].length) continue
+            var currCell = board[i][j]
+            var elNegCell = document.querySelector(`[data-i="${i}"][data-j="${j}"]`)
+            if (currCell.minesAroundCount === 0 && !currCell.isMine && currCell.isMarked && !currCell.isShown) {
+                elNegCell.classList.remove('hidden')
+                elNegCell.classList.add('clicked')
+                currCell.isShown = true
+                // //With Recursion
+                // if (currCell.minesAroundCount === 0) {
+                //     expandShown(board, elNegCell, i, j)
+                // }
             }
         }
     }
 }
-
-
-
-
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
